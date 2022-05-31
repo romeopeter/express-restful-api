670b2ed1be674e681ce9ccc38b44bcd71e51a061
romeo <romeomeshack34@gmail.com>
2022-05-31 10:31:25 +0100

    DB connection

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -4,12 +4,14 @@ import config from "config";
 export default async function connect() {
   const dbUri = config.get<string>("dbUri");
 
-    try {
-      await mongoose.connect(dbUri);
-    } catch (error) {
-        console.log("Could not connect to database");
-        
-        // Exit connection and indcate error 
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(dbUri);
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error);
+    console.log("Could not connect to database");
+      
+    // Exit connection and indcate error 
+    process.exit(1);
+  }
 }
