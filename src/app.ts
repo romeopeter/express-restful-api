43554523193c328bfaf8b6967eab7b74cd791304
romeo <romeomeshack34@gmail.com>
2022-06-23 14:09:48 +0100

    Brought in DB connection and routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import routes from "./routes";
 import config  from "config";
+import connect from "./utils/connect";
+import logger from "./utils/logger";
 
 const port = config.get<number>("port");
 const app = express();
 
 
-app.listen(1337, () => { 
-    console.log("App running on port",port);
+app.listen(1337, async () => { 
+    logger.info(`Server is running on http://localhost:${port}`);
+    
+    // Connects DB
+    // connect();
+
+    // URL Rotues
+    routes(app);
 })
