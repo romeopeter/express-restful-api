cf42ac862758f1c0edd98319318b52ce9e1b11d2
romeo <romeomeshack34@gmail.com>
2022-06-27 18:25:22 +0100

    User controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,13 +1,20 @@
-import {Request, Response} from "express";
-import log from "../utils/logger"
+import { Request, Response } from "express";
+import log from "../utils/logger";
+import { omit } from "lodash";
 import { createUser } from "../service/user.service";
+import { CreateUserInput } from "../schema/user.schema";
 
-export async function createUserHandler(req: Request, res: Response) {
-    try {
-        // Call service to create user
-       return await createUser(req.body);
-    } catch (e: any) {
-        log.error(e);
-        res.sendStatus(409).send(e.message);
-    }
+export async function createUserHandler(
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response
+) {
+  try {
+    // Call createUser service
+    const user = await createUser(req.body);
+    res.send(omit(user.toJSON(), "passoword"));
+  } catch (e: any) {
+    log.error(e);
+
+    return res.status(409).send(e.message);
+  }
 }
