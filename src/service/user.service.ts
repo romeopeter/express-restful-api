69cdab25bb76bfdefc11d53ba6a17f9bbde365c7
romeo <romeomeshack34@gmail.com>
2022-06-27 19:02:47 +0100

    Create usrer service

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,10 +1,12 @@
 import { DocumentDefinition } from "mongoose";
-import UserModel, {userDocument} from "../models/user.model";
+import UserModel, { userDocument } from "../models/user.model";
 
-export async function  createUser( input: DocumentDefinition<userDocument>) {
-    try {
-       return await UserModel.create(input);
-    } catch (e: any) {
-        throw new Error(e)
-    }
-} 
+export async function createUser(
+  input: DocumentDefinition<Omit<userDocument, "createdAt" | "updatedAt" | "comparePassword">>
+) {
+  try {
+    return await UserModel.create(input);
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
