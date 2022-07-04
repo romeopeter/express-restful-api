2b1a4e2f3d8a3c399b641a97ee57479a5304c3d6
romeo <romeomeshack34@gmail.com>
2022-07-04 11:17:15 +0100

    Server root

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,18 @@ import routes from "./routes";
 import config  from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
+import { json } from "body-parser";
 
 const port = config.get<number>("port");
 const app = express();
 
+app.use(express.json());
 
 app.listen(1337, async () => { 
     logger.info(`Server is running on http://localhost:${port}`);
     
-    // Connects DB
-    // connect();
+    // DB connection
+    connect();
 
     // URL Rotues
     routes(app);
