8e38cc2f72372b180483a7f0c07a1c9d7115b791
romeo <romeomeshack34@gmail.com>
2022-07-04 11:14:29 +0100

    Added session middleware

---
+++ src/schema/session.schema.ts
@@ -0,0 +1,13 @@
+import { boolean, object, string } from "zod";
+
+export const createSessionSchema = object({
+    body: object({
+        email: string({
+            required_error: "Email is required"
+        }
+        ),
+        password: string({
+            required_error: "Password is required"
+        })
+    }),
+})
