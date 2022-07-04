422e696d51b98c8545bf6b7de050e3ae260a5230
romeo <romeomeshack34@gmail.com>
2022-07-04 11:11:40 +0100

    Added service for validating password

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,12 +1,28 @@
+import { omit } from "lodash";
 import { DocumentDefinition } from "mongoose";
 import UserModel, { userDocument } from "../models/user.model";
 
 export async function createUser(
-  input: DocumentDefinition<Omit<userDocument, "createdAt" | "updatedAt" | "comparePassword">>
+  input: DocumentDefinition<
+    Omit<userDocument, "createdAt" | "updatedAt" | "comparePassword">
+  >
 ) {
   try {
     return await UserModel.create(input);
   } catch (e: any) {
     throw new Error(e);
   }
+}
+
+export async function validatePassword(email: string, password: string) {
+  const user = await UserModel.findOne({ email });
+
+  if (!user) return false;
+
+  // compare password
+  const passwordIsValid = await user.comparePassword(password);
+
+  if (!passwordIsValid) return false;
+
+  return omit(user, "password");
 }
